2a22f6e7634353fe6a888c6fe42575f5b9f57f4c
kennethtl <kennethtemba17@gmail.com>
2023-03-16 14:18:29 -0700

    Add 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Output to standard error)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
