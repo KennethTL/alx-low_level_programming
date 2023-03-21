cada10beafc40b63d182dd292e2c655948ab6e56
KennethTL <kennethtemba17@gmail.com>
2023-03-22 00:09:18 +0300

    Add 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @c: decimal value to be evaluated
+ *
+ * Return: last digit of c
+ */
+int print_last_digit(int c)
+{
+	int rval;
+
+	rval = c % 10;
+
+	return (rval);
+}
