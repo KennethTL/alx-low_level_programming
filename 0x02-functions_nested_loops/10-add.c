b3d0c9e13d7952ea2ffb073e5f4d719964f374ed
KennethTL <kennethtemba17@gmail.com>
2023-03-22 03:23:49 +0300

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first decimal parameter
+ * @b: second decimal parameter
+ *
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	int rval = a + b;
+
+	return (rval);
+}
