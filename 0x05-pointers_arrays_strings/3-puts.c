7adc34db2cfb11bb74bfb173093f70f61a5bfdcd
KennethTL <kennethtemba17@gmail.com>
2023-03-28 04:38:31 +0300

    Add 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int position;
+
+	while (str[position] != '\0')
+	{
+		_putchar(str[position]);
+		position++;
+	}
+
+	_putchar('\n');
+}
