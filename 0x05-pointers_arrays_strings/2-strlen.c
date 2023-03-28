42b290f76b082acf26532891a6758e0d5af0fdf0
KennethTL <kennethtemba17@gmail.com>
2023-03-28 03:44:45 +0300

    Add 2-dtrlrn.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to be counted
+ *
+ * Return: length of string s
+ */
+int _strlen(char *s)
+{
+	int size = sizeof(s) - 1;
+
+	return (size);
+}
