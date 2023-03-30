665522237a3890ab26cfbe743bf31f73f0497791
KennethTL <kennethtemba17@gmail.com>
2023-03-30 23:01:28 +0300

    Modify strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,28 +10,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int a1;
-	int a2;
-
-	for (i = 0; ; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
+		if (*s1 == '\0')
 		{
 			return (0);
 		}
-
-		a1 = (int) s1[i];
-		a2 = (int) s2[i];
-
-		if (a1 > a2)
-		{
-			return (15);
-		}
-
-		if (a2 > a1)
-		{
-			return (-15);
-		}
+		s1++;
+		s2++;
 	}
+
+	return (*s1 - *s2);
 }
