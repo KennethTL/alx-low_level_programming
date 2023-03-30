5542478ef457380b75df8d05717c5434f2514cc0
KennethTL <kennethtemba17@gmail.com>
2023-03-30 09:05:02 +0300

    Add task 0 deliverables

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the first part of the concatenated string (and its
+ * terminating null byte will be overridden)
+ * @src: the second part of the concatenated string that is copied to
+ * to the dest string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[j] = '\0';
+
+	return dest;
+}
