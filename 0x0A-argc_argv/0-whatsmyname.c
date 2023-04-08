8aff6c5da5610f40fb9f1719293f2198e47276aa
KennethTL <kennethtemba17@gmail.com>
2023-04-08 13:31:25 +0300

    Add task 0 deliverables

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for application
+ * @argc: a count of the arguments supplied to the program
+ * @argv: an array of pointers to the strings which are those args
+ * passed to the program by the system's CLI
+ *
+ * Return: 0 if successfull
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+
+	return (0);
+}
