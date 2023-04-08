9d8bfc930121ed15329b644c3510e2a38699ad48
KennethTL <kennethtemba17@gmail.com>
2023-04-08 14:38:06 +0300

    Modify function comment/doc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: an array of pointers to the strings which are those args
  * passed to the program by the system's CLI
  *
- * Return: 0 if successfull
+ * Return: 0 if successful, otherwise 1 if not successful
  */
 int main(int argc, char *argv[])
 {
