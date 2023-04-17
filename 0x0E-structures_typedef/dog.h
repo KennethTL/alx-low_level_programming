ad8b8e532f301a8b6f7baecd95bb6367b4145a6b
KennethTL <kennethtemba17@gmail.com>
2023-04-17 12:12:49 +0300

    Add task 0 deliverables

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a new type of struct dog
+ * *@name: name of the dog
+ * *@age: age of the dog
+ * *@owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
