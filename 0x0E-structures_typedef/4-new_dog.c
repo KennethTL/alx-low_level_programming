b818b6c374d984e36acc85f54c70e72a9f0d9d7e
KennethTL <kennethtemba17@gmail.com>
2023-04-17 19:06:47 +0300

    Modify condiiton handling function failure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_friend;
 
 	new_friend = malloc(sizeof(dog_t));
-	if (new_friend)
+	if (new_friend && name && owner && age > 0)
 	{
 		new_friend->name = name;
 		new_friend->age = age;
