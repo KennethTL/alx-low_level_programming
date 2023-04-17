cc244538c107619f2a89b2f91eb176c28154e3fe
KennethTL <kennethtemba17@gmail.com>
2023-04-17 13:52:04 +0300

    Add task 1 deliverables

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * *@d: variable of type struct dog
+ * *@name: name of the dog
+ * @age: age of the dog
+ * *@owner: name of the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
