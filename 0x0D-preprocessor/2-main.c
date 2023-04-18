16ad715425debaf42302e38c9484e07eb9275eaa
KennethTL <kennethtemba17@gmail.com>
2023-04-18 05:33:22 +0300

    Add task 2 deliverable

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for application
+ *
+ * Return: 0 if successful, otherwise 1 if not successful
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
