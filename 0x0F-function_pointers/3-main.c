b556b83a985b55d267d56ce3ac82408553ac764d
KennethTL <kennethtemba17@gmail.com>
2023-04-20 05:52:45 +0300

    Add task 3 deliverable

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - executes simple arithmetic operations
+ * @argc: argument count
+ * *@argv: array of arguments supplied by the system's CLI
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int operand1, operand2, solution;
+	char sign;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operand1 = atoi(argv[1]);
+	operand2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	sign = *argv[2];
+
+	if (operand2 == sign && (sign == '/' || sign == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	solution = operation(operand1, operand2);
+
+	printf("%d\n", solution);
+
+	return (0);
+}
