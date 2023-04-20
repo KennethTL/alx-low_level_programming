b4dd7c3aa620a0c342ba9b19e298afc7c548497a
KennethTL <kennethtemba17@gmail.com>
2023-04-20 05:38:02 +0300

    Modify task 3 deliverables

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * *get_op_func - selects the correct function to perform the op asked
+ * by the user
+ * @s: operation to be executed
+ *
+ * Return: pointer to the funciton of the selected operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
