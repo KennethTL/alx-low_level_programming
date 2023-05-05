700d7c203415697ffce6c35ddf6861765cfb4f6e
KennethTL <kennethtemba17@gmail.com>
2023-05-05 04:42:30 +0300

    Add task 5 deliverable

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip to get
+ * from one number to another
+ * @n: number before bits are flipped
+ * @m: number after bits are flipped
+ *
+ * Return: number of bits needed to be flipped
+ */
+unsigned int flip_bits(unsigned long n, unsigned long m)
+{
+	int total_bits;
+	unsigned long int difference;
+
+	difference = n ^ m;
+
+	for (; difference > 0; difference >>= 1)
+	{
+		if (difference & 1)
+		{
+			total_bits++;
+		}
+	}
+
+	return (total_bits);
+}
